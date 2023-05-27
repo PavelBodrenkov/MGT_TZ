b595b23271510dda0db802ca69af0e5fceadf20a
Павел Бодренков <bodrenkov_pv@ed-it.ru>
2023-05-27 16:25:54 +0300

    fix

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,16 +1,17 @@
 import './styles/index.scss';
-import React from 'react';
+import React, {Component} from 'react';
 import {Header} from "widgets/Header";
 import {Main} from "widgets/Main";
 
+class App extends Component {
+    render() {
+        return (
+            <div>
+                <Header />
+                <Main />
+            </div>
+        );
+    }
+}
 
-const App = () => {
-    return (
-        <div>
-           <Header />
-            <Main />
-        </div>
-    );
-};
-
-export default App;
+export default App
--- src/shared/ui/Watch/Watch.tsx
+++ src/shared/ui/Watch/Watch.tsx
@@ -1,24 +1,26 @@
 import {Component} from "react";
 import cls from './Watch.module.scss';
 
-
 export class Watch extends Component<any, any> {
-    constructor(props:any) {
+    constructor(props: any) {
         super(props);
         this.state = {time:''}
-        this.test = this.test.bind(this)
+        this.timer = this.timer.bind(this)
     }
 
-    test() {
+    timer() {
         let d = new Date();
         let s = d.getSeconds();
         let m = d.getMinutes();
         let h = d.getHours();
-        this.setState({time:("0" + h).substr(-2) + ":" + ("0" + m).substr(-2) + ":" + ("0" + s).substr(-2)})
+        this.setState({
+            time: ("0" + h).substr(-2) + ":" + ("0" + m).substr(-2) + ":" + ("0" + s).substr(-2)
+        })
     }
 
     componentDidMount() {
-        const timer =  setInterval(this.test, 1000);
+        this.timer()
+        const timer = setInterval(this.timer, 1000);
         return () => {
             clearInterval(timer)
         }
